3328e4f901d10d101c4c6f136564c01c6273f40d
Stepan Sendun <Stepan_Sendun@epam.com>
2023-04-21 09:19:01 +0300

    use fastify

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,20 @@ import { config } from 'dotenv';
 config();
 import { NestFactory } from '@nestjs/core';
 import { ConsoleLogger } from '@nestjs/common';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 import { APP_CONFIG } from './config';
 
 async function bootstrap() {
   const logger = new ConsoleLogger();
-  const app = await NestFactory.create(AppModule, {
-    logger,
-  });
+  const app = await NestFactory.create(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+    {
+      logger,
+    },
+  );
 
   await app.listen(APP_CONFIG.PORT, '0.0.0.0', () => {
     logger.log(`App is listening on http://locahost:${APP_CONFIG.PORT}`);
